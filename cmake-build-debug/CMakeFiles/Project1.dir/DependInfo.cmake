4f57f625c7aa7409be42a4a1e92484b90c8254b7
Anika Agarwal <anika.agarwal@yahoo.com>
2018-08-19 19:30:58 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/header.c" "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/cmake-build-debug/CMakeFiles/Project1.dir/header.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/Project1.cpp" "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/cmake-build-debug/CMakeFiles/Project1.dir/Project1.cpp.o"
+  "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/main.cpp" "/Users/Anika/Desktop/Programming Projects/Project 1 Spellcheck/Project1/cmake-build-debug/CMakeFiles/Project1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
